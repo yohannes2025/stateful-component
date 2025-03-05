65fbc5976eb0821b09f750014592291843f4b180
yohannes2025 <johannes.m.tekle@gmail.com>
2025-03-05 13:23:20 +0300

    Update

--- react-components/src/components/StatefulGreeting.js
+++ react-components/src/components/StatefulGreeting.js
@@ -1,31 +1,59 @@
-import React from "react";
+import React, { Component } from "react";
 
-class StatefulGreeting extends React.Component {
-  constructor(props){
+class StatefulGreeting extends Component {
+  constructor(props) {
     super(props);
     this.state = {
-      introduction: "Hello, I am a Stateful Component!", 
-      buttonText: "Exit"
+      introduction: "Hello!",
+      buttonText: "Exit",
+      count: 0,
     };
+
+    // Bind the methods to the instance
+    this.handleClick = this.handleClick.bind(this);
+    this.increment = this.increment.bind(this);
   }
-  handleClick(){
-    this.state({
-      introduction: "Goodbye!",
+
+  handleClick() {
+    this.setState(
+      {
+        introduction:
+          this.state.introduction === "Hello" ? "Goodbye!" : "Hello",
+        buttonText: this.state.buttonText === "Exit" ? "Enter" : "Exit",
+      },
+      () => {
+        console.log("new state", this.state.introduction);
+        console.log("new state", this.state.buttonText);
+      }
+    );
+
+    console.log(this.state.introduction); // This will not log the updated state immediately
+    console.log(this.state.buttonText); // This will not log the updated state immediately
+  }
+
+  increment() {
+    this.setState((prevState, prevPops) => {
+      console.log('Previous State:', prevState)
+      console.log('Previous State:', prevPops)
+      return{
+        count: prevState.count + 1,
+      }    
+      
     });
-    
-    console.log(this.state.introduction);
-
   }
+  
 
-  render(){
+  render() {
     return (
       <div>
         <h1>
           {this.state.introduction} {this.props.greeting}
         </h1>
-        <button onClick={() => this.handleClick()}> {this.state.buttonText}</button>
+        <button onClick={this.handleClick}>{this.state.buttonText}</button>
+        <button onClick={this.increment}>Increment</button>
+        <p>You have clicked {this.state.count} times</p>
       </div>
-    ); 
+    );
   }
 }
 
